490dda75f4ee6d07b214ba3c19cb9a15ca87a473
amit-singh88598 <amitsingh992728@gmail.com>
2021-02-23 19:26:51 +0530

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,29 @@
-import { createMuiTheme, ThemeProvider } from '@material-ui/core';
-import '../styles/globals.css'
+import { createMuiTheme, NoSsr, ThemeProvider } from '@material-ui/core';
+import '../styles/globals.css';
 
 const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: "#800000",
+	palette: {
+		primary: {
+			main: '#800000',
 
-      light: "#f6f0e2",
-      background: "#AE0000"
-
-
-    },
-    secondary: {
-      main: '#F2BA49',
-      white: "#ffffff"
-
-    }
-  },
+			light: '#f6f0e2',
+			background: '#AE0000'
+		},
+		secondary: {
+			main: '#F2BA49',
+			white: '#ffffff'
+		}
+	}
 });
 
 function MyApp({ Component, pageProps }) {
-  return <ThemeProvider theme={theme}>
-<Component {...pageProps} />
-  </ThemeProvider>
+	return (
+		<NoSsr>
+			<ThemeProvider theme={theme}>
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</NoSsr>
+	);
 }
 
-export default MyApp
+export default MyApp;
